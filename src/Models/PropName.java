d794dfc4c595f5d69ae07fbec939c9c5fd083682
Asaf Tobi <asaf.tobi@gmail.com>
2015-10-31 22:49:45 +0200

    Bs"d Adding annotations

---
+++ src/Models/PropName.java
@@ -0,0 +1,11 @@
+package Models;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface PropName {
+  String Name() default "";
+}
